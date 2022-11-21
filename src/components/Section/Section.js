b4022b13aa96ba7ae60871dc61bb38d013cf2600
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2022-11-21 14:59:36 +0200

    add propTypes

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,7 +1,12 @@
 import { Title } from './Section.styled';
+import PropTypes from 'prop-types';
 
 const Section = ({ title }) => {
   return <Title>{title}</Title>;
+};
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default Section;
